b2fb5dfc0579d5f99f25e71d868da4e832942eed
veserai <164884418+veserai@users.noreply.github.com>
2024-03-28 15:20:51 +0100

    Update index.tsx

--- app/components/chat/index.tsx
+++ app/components/chat/index.tsx
@@ -167,7 +167,7 @@ const Chat: FC<IChatProps> = ({
       {
         !isHideSendInput && (
           <div className={cn(!feedbackDisabled && '!left-3.5 !right-3.5', 'absolute z-10 bottom-0 left-0 right-0')}>
-            <div className='p-[5.5px] max-h-[150px] bg-white border-[1.5px] border-gray-200 rounded-xl overflow-y-auto'>
+            <div className='p-[5.5px] max-h-[150px] bg-green border-[1.5px] border-gray-200 rounded-xl overflow-y-auto'>
               {
                 visionConfig?.enabled && (
                   <>
