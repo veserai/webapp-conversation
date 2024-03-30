7cbd6027cb24b66dcd9a3331eaf25f65069d5247
veserai <164884418+veserai@users.noreply.github.com>
2024-03-30 21:30:10 +0100

    Update index.tsx

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -16,7 +16,7 @@ import type { Emoji } from '@/types/tools'
 
 const OperationBtn = ({ innerContent, onClick, className }: { innerContent: React.ReactNode; onClick?: () => void; className?: string }) => (
   <div
-    className={`relative box-border flex items-center justify-center h-7 w-7 p-0.5 rounded-lg bg-green cursor-pointer text-gray-500 hover:text-gray-800 ${className ?? ''}`}
+    className={`relative box-border flex items-center justify-center h-7 w-7 p-0.5 rounded-lg bg-green-100 cursor-pointer text-gray-500 hover:text-gray-800 ${className ?? ''}`}
     style={{ boxShadow: '0px 4px 6px -1px rgba(0, 0, 0, 0.1), 0px 2px 4px -2px rgba(0, 0, 0, 0.05)' }}
     onClick={onClick && onClick}
   >
