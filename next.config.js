c0b9625006bd73a0ab894c750db65912b7fca03e
veserai <164884418+veserai@users.noreply.github.com>
2024-04-08 07:41:40 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/v1/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
