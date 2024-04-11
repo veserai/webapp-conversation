53c5cafd40bc673908b26dd33615b650857a2d7a
veserai <164884418+veserai@users.noreply.github.com>
2024-04-11 14:56:15 +0200

    Update index.tsx

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -37,7 +37,7 @@ const Main: FC = () => {
   const [promptConfig, setPromptConfig] = useState<PromptConfig | null>(null)
   const [inited, setInited] = useState<boolean>(false)
   // in mobile, show sidebar by click button
-  const [isShowSidebar, { setTrue: showSidebar, setFalse: hideSidebar }] = useBoolean(false)
+  const [isShowSidebar, { setTrue: showSidebar, setFalse: hideSidebar }] = useBoolean(true)
   const [visionConfig, setVisionConfig] = useState<VisionSettings | undefined>({
     enabled: false,
     number_limits: 2,
