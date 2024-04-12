b1c039f4ef21374e9ded5504ca25657d4f42848b
veserai <164884418+veserai@users.noreply.github.com>
2024-04-12 15:00:16 +0200

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -82,10 +82,10 @@ const Welcome: FC<IWelcomeProps> = ({
 
   const renderHeader = () => {
     return ()
-      //<div className='absolute top-0 left-0 right-0 flex items-center justify-between border-b border-gray-100 mobile:h-12 tablet:h-16 px-8 bg-green-50'>
-        //<div className='text-gray-900'>{conversationName}</div>
-      //</div>
-    //)
+      <div className='absolute top-0 left-0 right-0 flex items-center justify-between border-b border-gray-100 mobile:h-12 tablet:h-16 px-8 bg-green-50'>
+        <div className='text-green-50'>{conversationName}</div>
+      </div>
+    )
   }
 
   const renderInputs = () => {
